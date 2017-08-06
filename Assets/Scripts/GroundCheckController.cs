b07f38f51b03448cd154ed6d0a2d9753d06370bd
jamiepollock <jamie.does.umbraco@gmail.com>
2017-08-06 11:30:20 -0400

    Reorganised project, added exit and grounded to platforms

---
+++ Assets/Scripts/GroundCheckController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckController : MonoBehaviour {
+    public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+	}
+    
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (transform.position.y < collision.otherCollider.transform.position.y)
+        //{
+        //if (collision.CompareTag("GroundZone"))
+        //{
+            player.isGrounded = true;
+        //}
+        //}
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        player.isGrounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        player.isGrounded = false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,44 +6,26 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
     public float jumpPower;
     public float speed;
-    private bool groundCheck;
+    [HideInInspector] public bool isGrounded;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        groundCheck = false;
+        isGrounded = false;
     }
 
     void FixedUpdate()
     {
+        var askedToJump = Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump");
+
         float moveHorizontal = Input.GetAxis("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
-
-        if (Input.GetKeyDown(KeyCode.Space) && groundCheck)
+        if (askedToJump && isGrounded)
         {
             rb2d.AddForce(Vector2.up * jumpPower);
         }
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal, 0);
 
         rb2d.AddForce(movement * speed);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        groundCheck = true;
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        groundCheck = false;
-    }
-
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        groundCheck = true;
     }
 }
