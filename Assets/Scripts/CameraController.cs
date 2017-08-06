ec2f9d6e5cd843ff34d375e95318a08662a60505
jamiepollock <jamie.does.umbraco@gmail.com>
2017-08-06 07:28:07 -0400

    Basic follow camera and ground check

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
+    public float jumpPower;
+    public float speed;
+    private bool groundCheck;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-	}
+        groundCheck = false;
+    }
 
     void FixedUpdate()
     {
@@ -17,9 +21,29 @@ public class PlayerController : MonoBehaviour {
         //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis("Vertical");
 
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+        }
+
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        rb2d.AddForce(movement * 10);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        groundCheck = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        groundCheck = false;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        groundCheck = true;
     }
 }
